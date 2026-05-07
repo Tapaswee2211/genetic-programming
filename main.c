f9a3bf85c5e0da084bd5c16b8c4c87ba527ad4b5
Tapaswee2211 <dasaritapaswee2018@gmail.com>
2026-05-08 01:43:32 +0530

    window rendered using SDL2

--- main.c
+++ main.c
@@ -1,8 +1,56 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<SDL2/SDL.h>
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+
+
+int scc(int code){
+  if (code <0 ) { 
+    fprintf(stderr, "SDL Error : %s\n", SDL_GetError());
+    exit(1);
+  }
+  return code;
+}
+
+void *scp(void *ptr) {
+  if (ptr == NULL) {
+    fprintf(stderr, "SDL Error : %s\n", SDL_GetError());
+    exit(1);
+  }
+  return ptr;
+}
 
 int main (int argc, char * argv[])
 {
-  printf("Hello, World!\n");
+  scc(SDL_Init(SDL_INIT_VIDEO));
+  SDL_Window * const window = scp(SDL_CreateWindow("Game1",
+                                       0, 0, 
+                                       SCREEN_WIDTH, SCREEN_HEIGHT, 
+                                       SDL_WINDOW_RESIZABLE));
+
+
+  SDL_Renderer *const  renderer =  scp(SDL_CreateRenderer(window,
+                                                      -1, 
+                                                      SDL_RENDERER_ACCELERATED ));
+  int quit= 0;
+  while (!quit) {
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) { 
+      switch(event.type) {
+        case SDL_QUIT: {
+          quit= 1;
+        }break;
+      }
+
+      scc(SDL_SetRenderDrawColor(renderer, 30, 30, 30, 255));
+      scc(SDL_RenderClear(renderer));                              
+      SDL_RenderPresent(renderer);
+    }
+
+  }
+  SDL_Quit();
+  printf("ran\n");
   return 0;
 }
